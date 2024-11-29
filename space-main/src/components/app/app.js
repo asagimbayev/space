9366720209746697337de71a48d5110708c66c2c
Anuar <sagimbayev01@gmail.com>
2024-11-29 17:30:53 +0500

    Step 8

---
+++ space-main/src/components/app/app.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+
+import Header from '../header'
+import RandomPlanet from '../random-planet'
+import ItemList from '../item-list'
+import PersonDetails from '../person-details'
+
+import './app.css'
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
+import SwapiService from "../../services/swapi-service";
+
+export default class App extends Component{
+
+    swapiService = new SwapiService()
+
+    state = {
+        selectedPerson: null,
+        hasError: false
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true })
+    }
+
+    render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        return (
+            <div>
+                <Header/>
+                <RandomPlanet/>
+
+                <PeoplePage />
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={ this.swapiService.getAllPlanets }/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={ this.swapiService.getAllStarships }/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
